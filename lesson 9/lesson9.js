2f0e3d7a9972ef6679b3cdb58558adc9c65acbef
Oleksii Mosunov <alexmosunov@gmail.com>
2019-10-10 21:07:21 +0300

    putting everything into folders

---
+++ lesson 9/lesson9.js
@@ -0,0 +1,26 @@
+//OBJECTS
+
+// task 1
+
+let productObject = {
+    product: 'iphone'
+}
+
+// task 2 
+
+productObject.price = 1000;
+productObject.currency = 'dollar';
+
+// task 3
+
+productObject.details = {};
+productObject.details.model = '';
+productObject.details.color = '';
+
+
+
+
+
+
+
+console.log(productObject);
