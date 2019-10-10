3e9f40ea771de21d5d13de006f55fab81002888a
Oleksii Mosunov <alexmosunov@gmail.com>
2019-10-10 19:34:12 +0300

    Adding lesson 6 homework

---
+++ lesson6.js
@@ -0,0 +1,40 @@
+// NUMBERS
+// task 1
+let numberPI = +Math.PI.toFixed(2);
+console.log(numberPI);
+
+// task 2
+let maxNumber = Math.max(15, 11, 16, 12, 51, 12, 13, 51);
+let minNumber = Math.min(15, 11, 16, 12, 51, 12, 13, 51);
+console.log(maxNumber);
+console.log(minNumber);
+
+
+// task 3a
+
+let randomNumber = +Math.random().toFixed(2);
+console.log(randomNumber);
+
+// task 3b
+let x = Math.random();
+let randomNumber2 = (Math.random() * x);
+
+console.log(x, randomNumber2);
+
+// task 4
+let resultSum = Math.round((0.6 + 0.7) * 10) / 10;
+console.log(resultSum);
+
+// task 5
+let dollarString = '100$'
+let dollarToNumber = parseFloat(dollarString);
+console.log(dollarToNumber);
+
+
+
+
+
+
+
+
+
